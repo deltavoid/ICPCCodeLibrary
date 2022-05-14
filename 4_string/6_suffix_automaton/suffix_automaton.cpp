30a49a6582a382a400a3b26640d6dc5984440b1d
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2022-05-15 01:24:06 +0800

    aupdate suffix automaton

--- 4_string/6_suffix_automaton/suffix_automaton.cpp
+++ 4_string/6_suffix_automaton/suffix_automaton.cpp
@@ -1,6 +1,5 @@
-//CF666E
-//SAM+SegTree_class
-
+// CF666E
+// 后缀自动机， 倍增法， 可持久化线段树
 #include<cstdio>
 #include<cstdlib>
 #include<cstring>
@@ -8,6 +7,7 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
+
 
 class SAM
 {public:
@@ -74,41 +74,83 @@ struct Node
     int s1, s2;
 };
 
-const int S = SAM::N;
-Node T[S*23+10];
-int cnt, roots[S+10];
+class SegTree {
+public:
 
-void Insert(int& x, int val, int l, int r)
-{
-    if  (!x)  x = ++cnt;
-    if  (l == r)  {  T[x].key.val++;  T[x].key.id = l;  return ;}
-    int mid = (l + r) >> 1;
-    if  (val <= mid)  Insert(T[x].s1, val, l, mid);
-    else Insert(T[x].s2, val, mid+1, r);
-    T[x].key = T[T[x].s1].key * T[T[x].s2].key;
-}
+    static const int S = SAM::N;
 
-int Merge(int x, int y, int l, int r)
-{
-    if  (!x || !y)  return x | y;
-    int z = ++cnt;
-    if  (l == r)  {  T[z].key = T[x].key + T[y].key;  return z;}
-    int mid = (l + r) >> 1;
-    T[z].s1 = Merge(T[x].s1, T[y].s1, l, mid);
-    T[z].s2 = Merge(T[x].s2, T[y].s2, mid + 1, r);
-    T[z].key = T[T[z].s1].key * T[T[z].s2].key;
-    return z;
-}
+    Node T[S*23+10];
+    int cnt, roots[S+10];
 
-Key Query(int x, int l, int r, int  L, int R)
-{
-    if  (!x)  return Key(0, 0);
-    if  (L <= l && R >= r)  return T[x].key;
-    int mid = (l + r) >> 1;
-    if  (R <= mid)  return Query(T[x].s1, l, mid, L, R);
-    else if  (L > mid)  return Query(T[x].s2, mid + 1, r, L, R);
-    else return Query(T[x].s1, l, mid, L, mid) * Query(T[x].s2, mid + 1, r, mid + 1, R);
-}
+    SegTree() 
+    {
+        cnt = 0;
+        memset(T, 0, sizeof(T));
+        memset(roots, 0, sizeof(roots));
+    }
+
+    void Insert(int& x, int val, int l, int r)
+    {
+        if  (!x)  x = ++cnt;
+        if  (l == r)  {  T[x].key.val++;  T[x].key.id = l;  return ;}
+        int mid = (l + r) >> 1;
+        if  (val <= mid)  Insert(T[x].s1, val, l, mid);
+        else Insert(T[x].s2, val, mid+1, r);
+        T[x].key = T[T[x].s1].key * T[T[x].s2].key;
+    }
+
+    int Merge(int x, int y, int l, int r)
+    {
+        if  (!x || !y)  return x | y;
+        int z = ++cnt;
+        if  (l == r)  {  T[z].key = T[x].key + T[y].key;  return z;}
+        int mid = (l + r) >> 1;
+        T[z].s1 = Merge(T[x].s1, T[y].s1, l, mid);
+        T[z].s2 = Merge(T[x].s2, T[y].s2, mid + 1, r);
+        T[z].key = T[T[z].s1].key * T[T[z].s2].key;
+        return z;
+    }
+
+    Key Query(int x, int l, int r, int  L, int R)
+    {
+        if  (!x)  return Key(0, 0);
+        if  (L <= l && R >= r)  return T[x].key;
+        int mid = (l + r) >> 1;
+        if  (R <= mid)  return Query(T[x].s1, l, mid, L, R);
+        else if  (L > mid)  return Query(T[x].s2, mid + 1, r, L, R);
+        else return Query(T[x].s1, l, mid, L, mid) * Query(T[x].s2, mid + 1, r, mid + 1, R);
+    }
+};
+
+SegTree segtree;
+
+class SAMFather {
+public:
+
+    SAM& sam;
+    int father[SAM::N + 10][21];
+
+    SAMFather(SAM& sam) : sam(sam) {}
+
+    void build()
+    {
+        for (int i = 1; i <= sam.size; i++)
+            father[i][0] = sam.par[i];
+        for (int k = 1; k <= 20; k++)
+            for (int i = 1; i <= sam.size; i++)
+                father[i][k] = father[ father[i][k-1] ][k-1];
+    }
+
+    int get(int x, int len)
+    {
+        for (int i = 20; i >= 0; i--)
+            if  (sam.len[ father[x][i] ] >= len)
+                x = father[x][i];
+        return x;
+    }
+};
+
+SAMFather father(sam);
 
 
 const int N = 500000;
@@ -117,23 +159,9 @@ char str1[N+10], str2[N+10];
 int pos[N+10];
 
 
-int father[S+10][21];
-
-int Get(int l, int r)
-{
-    int Len = r - l + 1, x = pos[r];
-    for (int i = 20; i >= 0; i--)
-        if  (sam.len[ father[x][i] ] >= Len)
-            x = father[x][i];
-    return x;
-}
-
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+
     sam.init();
 
     scanf("%s", str1+1);  n = strlen(str1+1);
@@ -145,7 +173,7 @@ int main()
     for (int i = 1; i <= m; i++)
     {   scanf("%s", str2+1);  int l = strlen(str2+1);
         for (int j = 1; j <= l; j++)
-            sam.extend(str2[j] - 'a'), Insert(roots[sam.last], i, 1, m);
+            sam.extend(str2[j] - 'a'), segtree.Insert(segtree.roots[sam.last], i, 1, m);
         sam.extend(26);
     }
     
@@ -153,21 +181,17 @@ int main()
     
     for (int i = sam.size; i >= 1; i--)
     {   int x = sam.id[i], p = sam.par[x];
-        if  (p)  roots[p] = Merge(roots[p], roots[x], 1, m);
+        if  (p)  segtree.roots[p] = segtree.Merge(segtree.roots[p], segtree.roots[x], 1, m);
     }
 
-    for (int i = 1; i <= sam.size; i++)
-        father[i][0] = sam.par[i];
-    for (int k = 1; k <= 20; k++)
-        for (int i = 1; i <= sam.size; i++)
-            father[i][k] = father[ father[i][k-1] ][k-1];
+    father.build();
 
     scanf("%d", &q);
     int l, r, pl, pr;
     for (int k = 1; k <= q; k++)
     {   scanf("%d%d%d%d", &l, &r, &pl, &pr);
-        int x = Get(pl, pr);
-        Key ans = Query(roots[x], 1, m, l, r);
+        int x = father.get(pos[pr], pr - pl + 1);
+        Key ans = segtree.Query(segtree.roots[x], 1, m, l, r);
         if  (!ans.val)  printf("%d %d\n", l, 0);
         else printf("%d %d\n", ans.id, ans.val);
     }
